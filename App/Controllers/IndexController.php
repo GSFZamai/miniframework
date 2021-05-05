713c1de33c529165802bda072307da77b4b71542
gsfzamai <gsfzamai@gmail.com>
2021-05-05 15:21:51 -0300

    miniframework

---
+++ App/Controllers/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+
+    namespace App\Controllers;
+
+    class IndexController {
+
+        public function index() {
+            echo 'Chegamos até o IndexController e acessamos a rota index';
+        }
+
+        public function sobreNos() {
+            echo 'Chegamos até o IndexController e acessamos a rota sobreNos';
+        }
+    }
+
+?>
