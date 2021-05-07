808596365d37ef4c18305cd884997892e2db9f73
gsfzamai <gsfzamai@gmail.com>
2021-05-07 13:46:51 -0300

    finalizado o miniframework

--- App/Controllers/IndexController.php
+++ App/Controllers/IndexController.php
@@ -3,16 +3,30 @@
     namespace App\Controllers;
 
     use MF\Controller\Action;
+    use MF\Model\Container;
 
     class IndexController extends Action{
 
         public function index() {
-            $this->view->dados = array('Caldo Verde', 'Caldo Detox', 'Caldo Laranja');
+            //$this->view->dados = array('Caldo Verde', 'Caldo Detox', 'Caldo Laranja');
+            $produto = Container::getModel('Produto');   
+            
+            $produtos = $produto->getProdutos();
+
+            $this->view->dados = $produtos;
+
             $this->render('index', 'layout1');
         }
 
         public function sobreNos() {
-            $this->view->dados = array('Batata', 'Cenoura', 'Cebola');
+            //$this->view->dados = array('Batata', 'Cenoura', 'Cebola');
+            
+            $info = Container::getModel('Info');
+
+            $infos = $info->getInfos();
+
+            $this->view->dados = $infos;
+
             $this->render('sobreNos', 'layout2');
         }
 
